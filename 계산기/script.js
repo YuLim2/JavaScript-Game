61cde39d69b1b6da92eb627a6aedde89a25ae13c
YuLim2 <80025122+YuLim2@users.noreply.github.com>
2021-10-20 19:05:48 +0900

    계산기 - git허브에서 계산기 html, css 퍼온 후 내 틀에 맞게 수정함, js에서 기본 선언 참고 - https://github.com/abarna-codespot/A-simple-Calculator

---
+++ 계산기/script.js
@@ -0,0 +1,11 @@
+var num1 = '';
+var operater = '';
+var num2 = '';
+var button = [];
+var $operater = document.querySelector('#operater');
+var $result = document.querySelector('#result');
+for (var i = 0; i<9; i++) {
+    button[i].addEventListener('click', function() {
+        
+    })
+}
