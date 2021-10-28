3532204c7948623d618308beb9c2bf80dedbbf8a
YuLim2 <80025122+YuLim2@users.noreply.github.com>
2021-10-28 20:47:58 +0900

    숫자 버튼 클릭시 내용 값 전달, 연산자 버튼 클릭시 연산자 전달

--- 계산기/script.js
+++ 계산기/script.js
@@ -1,11 +1,39 @@
-var num1 = '';
-var operater = '';
-var num2 = '';
+var numOne = '';
+var operator = '';
+var numTwo = '';
 var button = [];
-var $operater = document.querySelector('#operater');
+var $operator = document.querySelector('#operator');
 var $result = document.querySelector('#result');
-for (var i = 0; i<9; i++) {
-    button[i].addEventListener('click', function() {
-        
-    })
-}
+
+var onClickNumber = (event) => {  //숫자를 눌렀을 때
+    if(operator){
+        numTwo += event.target.textContent;     //event.target.textContent 태그 안에 내용을 가져옴
+    } else{
+        numOne += event.target.textContent;
+    }
+    $result.value += event.target.textContent;
+};
+
+var onClickOperator = (op) => () => {   //연산을 눌렀을 때
+    if(numOne){
+        operator = op;  
+        $operater.value = op;   //operator.value == operator
+    } else {
+        alert('숫자를 입력하고 눌러라!!!');
+    }
+};
+
+document.querySelector('#num-0').addEventListener('click', onClickNumber);
+document.querySelector('#num-1').addEventListener('click', onClickNumber);
+document.querySelector('#num-2').addEventListener('click', onClickNumber);
+document.querySelector('#num-3').addEventListener('click', onClickNumber);
+document.querySelector('#num-4').addEventListener('click', onClickNumber);
+document.querySelector('#num-5').addEventListener('click', onClickNumber);
+document.querySelector('#num-6').addEventListener('click', onClickNumber);
+document.querySelector('#num-7').addEventListener('click', onClickNumber);
+document.querySelector('#num-8').addEventListener('click', onClickNumber);
+document.querySelector('#num-9').addEventListener('click', onClickNumber);
+document.querySelector('#num-plus').addEventListener('click', onClickOperator('+'));
+document.querySelector('#num-minus').addEventListener('click', onClickOperator('-'));
+document.querySelector('#num-divide').addEventListener('click', onClickOperator('/'));
+document.querySelector('#num-multiply').addEventListener('click', onClickOperator('*'));
